f14753b888fec3370b75ef37c59ca626c6a0a6a9
Lucas Goyeche <lucas@raxel.co>
2024-06-25 01:49:51 -0300

    style: improve command palette positioning

--- src/components/ui/dialog/DialogContent.vue
+++ src/components/ui/dialog/DialogContent.vue
@@ -30,13 +30,13 @@ const PortalTarget = ref()
   <div ref="PortalTarget" />
   <DialogPortal :to="PortalTarget">
     <DialogOverlay
-      class="nsc-fixed nsc-inset-0 nsc-z-50 nsc-bg-black/80  data-[state=open]:nsc-animate-in data-[state=closed]:nsc-animate-out data-[state=closed]:nsc-fade-out-0 data-[state=open]:nsc-fade-in-0"
+      class="nsc-fixed nsc-inset-0 nsc-z-50 nsc-bg-black/60"
     />
     <DialogContent
       v-bind="forwarded"
       :class="
         cn(
-          'nsc-fixed nsc-left-1/2 nsc-top-1/2 nsc-z-50 nsc-grid nsc-w-full nsc-max-w-lg -nsc-translate-x-1/2 -nsc-translate-y-1/2 nsc-gap-4 nsc-border nsc-bg-background nsc-shadow-lg nsc-duration-200 data-[state=open]:nsc-animate-in data-[state=closed]:nsc-animate-out data-[state=closed]:nsc-fade-out-0 data-[state=open]:nsc-fade-in-0 data-[state=closed]:nsc-zoom-out-95 data-[state=open]:nsc-zoom-in-95 data-[state=closed]:nsc-slide-out-to-left-1/2 data-[state=closed]:nsc-slide-out-to-top-[48%] data-[state=open]:nsc-slide-in-from-left-1/2 data-[state=open]:nsc-slide-in-from-top-[48%] sm:nsc-rounded-lg',
+          'nsc-content nsc-fixed nsc-left-1/2 nsc-top-[150px] -nsc-translate-x-1/2 nsc-z-50 nsc-grid nsc-w-full nsc-max-w-xl nsc-gap-4 nsc-border nsc-bg-background nsc-shadow-lg nsc-duration-20 sm:nsc-rounded-lg',
           props.class,
         )"
     >
@@ -51,3 +51,13 @@ const PortalTarget = ref()
     </DialogContent>
   </DialogPortal>
 </template>
+
+<style>
+.nsc-content {
+  -webkit-font-smoothing: subpixel-antialiased;
+  -webkit-filter: blur(0px);
+  -webkit-perspective: 1000;
+  filter: blur(0px);
+  backface-visibility: hidden;
+}
+</style>
