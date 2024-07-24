55ebbf58bf5a24a4fd5a6f8f04883e2d55befa44
Lucas Goyeche <lucas@raxel.co>
2024-07-23 22:43:33 -0300

    fix: hide DialogContent if dialog is hidden

--- src/components/ui/command/CommandDialog.vue
+++ src/components/ui/command/CommandDialog.vue
@@ -14,7 +14,7 @@ const forwarded = useForwardPropsEmits(props, emits)
 
 <template>
   <Dialog v-bind="forwarded">
-    <DialogContent class="nsc-overflow-hidden nsc-p-0 nsc-shadow-lg">
+    <DialogContent v-if="props.open" class="nsc-overflow-hidden nsc-p-0 nsc-shadow-lg">
       <Command v-bind="forwarded" class="[&_[cmdk-group-heading]]:nsc-px-2 [&_[cmdk-group-heading]]:nsc-font-medium [&_[cmdk-group-heading]]:nsc-text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:nsc-pt-0 [&_[cmdk-group]]:nsc-px-2 [&_[cmdk-input-wrapper]_svg]:nsc-h-5 [&_[cmdk-input-wrapper]_svg]:nsc-w-5 [&_[cmdk-input]]:nsc-h-12 [&_[cmdk-item]]:nsc-px-2 [&_[cmdk-item]]:nsc-py-3 [&_[cmdk-item]_svg]:nsc-h-5 [&_[cmdk-item]_svg]:nsc-w-5">
         <slot />
       </Command>
